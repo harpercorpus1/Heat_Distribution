e6f6db273dbc7e12eef786ab0096aca0d4f0e078
ecorpus <ecorpus@clemson.edu>
2022-02-26 23:39:02 -0500

    update

--- seq_script.sh
+++ seq_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 gcc as2_seq.c -o as2_seq
-for VAR in 10 100 1000 10000
+for VAR in 10 100 1000 10000 100000
 do
-	./as2_seq $VAR
-	cp as2.png as2_seq_$VAR.png
+	perf stat -o perf_seq_${VAR}.txt ./as2_seq ${VAR}
+	cp as2.png as2_seq_${VAR}.png
 	rm as2.png
 	rm as2.pnm
 done
